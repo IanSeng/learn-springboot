dfc4ce8c4f35f7f7e56bc2e8dd5899a693bf7b6f
Ian Seng <ianseng@outlook.com>
2020-05-25 19:54:06 -0400

    new springboot camp learn

---
+++ springboot-camp/src/main/java/com/example/springbootcamp/SpringbootCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCampApplication.class, args);
+	}
+
+}
