9ca08e216872ecc58c046823d1a171178c9b9bd4
Ian Seng <ianseng@outlook.com>
2020-05-26 18:44:34 -0400

    completed client side api

--- springboot-camp/src/main/java/com/example/springbootcamp/api/PersonController.java
+++ springboot-camp/src/main/java/com/example/springbootcamp/api/PersonController.java
@@ -3,10 +3,12 @@ package com.example.springbootcamp.api;
 import com.example.springbootcamp.modal.Person;
 import com.example.springbootcamp.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -20,7 +22,28 @@ public class PersonController {
   }
 
   @PostMapping
-  public void addPerson(@RequestBody Person person) {
+  public void addPerson(@Valid @NotNull @RequestBody Person person) {
     personService.addPerson(person);
   }
+
+  @GetMapping
+  public List<Person> getAllPeople() {
+    return personService.getAllPeople();
+  }
+
+  @GetMapping(path = "{id}")
+  public Person getPersonById(@PathVariable("id") UUID id) {
+    return personService.getPersonById(id).orElse(null);
+  }
+
+  @DeleteMapping(path = "{id}")
+  public void deletePersonById(@PathVariable("id") UUID id) {
+    personService.deletePerson(id);
+  }
+
+  @PutMapping(path = "{id}")
+  public void updatePerson(@PathVariable("id") UUID id, @Valid @NotNull @RequestBody Person personToUpdate) {
+    personService.updatePerson(id, personToUpdate);
+  }
+
 }
